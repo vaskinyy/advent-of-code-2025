42d5db92f1de2dd13d63cb168e263de2a78b31f1
vaskinyy <yuri.vaskin@thetrainline.com>
2025-12-04 09:23:08 +0000

    Day 4

---
+++ day_4/1.py
@@ -0,0 +1,40 @@
+from models.board import get_coords_around
+from parsers.input_parser import InputParser
+
+test_mode = False
+
+parser = InputParser(test_mode)
+parser.parse()
+
+parser.build_boards()
+
+board = parser.get_board()
+items = board.items
+width = board.width
+height = board.height
+
+num_items = 0
+
+items_to_remove = [(-1, -1)]
+
+while items_to_remove:
+    for (i, j) in items_to_remove:
+        if (i, j) == (-1, -1):
+            continue
+        items[i][j] = "."
+    items_to_remove = []
+
+    for i in range(height):
+        for j in range(width):
+            if items[i][j] != "@":
+                continue
+            coords = get_coords_around(i, j, height, width)
+            counter = 0
+            for near_i, near_j in coords:
+                if items[near_i][near_j] == "@":
+                    counter += 1
+            if counter < 4:
+                items_to_remove.append((i, j))
+                num_items += 1
+
+print(num_items)
