f0cb8037bf8112c4329ac548c2bf8feac5b227d2
vaskinyy <yuri.vaskin@thetrainline.com>
2025-12-15 20:40:06 +0000

    Day 10.2

--- day_10/2.py
+++ day_10/2.py
@@ -1,62 +1,53 @@
-from itertools import combinations
+from collections import defaultdict
 
 from parsers.input_parser import InputParser
+import z3
 
 test_mode = False
 
 parser = InputParser(test_mode)
 parser.parse()
 
-lines = [line.split(',') for line in parser.get_input_lines()]
-coords = [(int(i), int(j)) for i, j in lines]
+lines = [line.split(' ') for line in parser.get_input_lines()]
 
-coord_set = set(coords)
+lights = []
+buttons = []
+indicators = []
 
-pairs = [(coord1, coord2) for coord1, coord2 in combinations(coords, 2)]
+for i, line in enumerate(lines):
+    lights.append([])
+    buttons.append([])
+    indicators.append([])
+    for item in line:
+        if item.startswith('['):
+            lights[i].append(item[1:-1])
+        elif item.startswith('('):
+            cleaned_items = [int(el) for el in item[1:-1].split(',')]
+            buttons[i].append(cleaned_items)
+        elif item.startswith('{'):
+            cleaned_items = [int(el) for el in item[1:-1].split(',')]
+            indicators[i].append(cleaned_items)
 
-areas = []
+sums = []
+for cur_lights, cur_buttons, cur_indicators in zip(lights, buttons, indicators):
+    cur_indicator = cur_indicators[0]
 
+    solver = z3.Optimize()
+    button_states = z3.IntVector("button_states", len(cur_buttons))
+    button_indices = defaultdict(list)
+    for i, cur_button in enumerate(cur_buttons):
+        solver.add(button_states[i] >= 0)
+        for j in cur_button:
+            button_indices[j].append(i)
 
-def coords_to_fill(coord1, coord2, existing_coords):
-    x1, y1 = coord1
-    x2, y2 = coord2
+    for i, idxes in button_indices.items():
+        solver.add(cur_indicator[i] == sum(button_states[j] for j in idxes))
 
-    if x1 == x2:
-        lower, upper = sorted((y1, y2))
-        return {(x1, y) for y in range(lower + 1, upper)}
+    presses = z3.Sum(button_states)
+    solver.minimize(presses)
+    solver.check()
+    sums.append(solver.model().eval(presses).as_long())
 
-    if y1 == y2:
-        left, right = sorted((x1, x2))
-        return {(x, y1) for x in range(left + 1, right)}
+print(sums)
+print(sum(sums))
 
-    other_corners = [(x1, y2), (x2, y1)]
-    if not all(corner in existing_coords for corner in other_corners):
-        return set()
-
-    x_start, x_end = sorted((x1, x2))
-    y_start, y_end = sorted((y1, y2))
-
-    if x_end - x_start <= 1 or y_end - y_start <= 1:
-        return set()
-
-    return {
-        (x, y)
-        for x in range(x_start + 1, x_end)
-        for y in range(y_start + 1, y_end)
-    }
-
-
-for coord1, coord2 in pairs:
-    to_fill = coords_to_fill(coord1, coord2, coord_set)
-    if not to_fill:
-        continue
-
-    if any(point in coord_set for point in to_fill):
-        continue
-
-    # print((coord1[1], coord1[0]), (coord2[1], coord2[0]))
-    print(coord1, coord2)
-    coord_set.update(to_fill)
-    areas.append(abs(coord1[0] - coord2[0] + 1) * abs(coord1[1] - coord2[1] + 1))
-
-print(sorted(areas))
